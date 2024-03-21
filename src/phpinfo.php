2f984134817e87a4e24f8c17d4d8815d18e77ba6
Amaury Gz <gzamaury@hotmail.com>
2024-03-20 20:20:41 -0600

    add: phpinfo.pho file to get the PHP configuration

---
+++ src/phpinfo.php
@@ -0,0 +1,22 @@
+<?php
+
+function sayHello($name) {
+	echo "Hello $name!";
+}
+
+?>
+
+<html>
+	<head>
+		<title>Visual Studio Code Remote :: PHP</title>
+	</head>
+	<body>
+		<?php 
+		
+		sayHello('from phpinfo.php and phpinfo()');
+			
+		phpinfo(); 
+			
+		?>
+	</body>
+</html>

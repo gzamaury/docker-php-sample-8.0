acb3735a6614d3366b03c185c54ba52011e9ec96
Amaury Gz <gzamaury@hotmail.com>
2024-03-29 01:51:08 +0000

    add: wire the src projecto folder to the /var/www/html folder, to start Apache use:
      apache2ctl start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,3 +8,8 @@ composer update
 echo "Installing Composer dependencies for production."
 export COMPOSER_VENDOR_DIR=./vendor
 composer install --no-dev --no-interaction
+
+# Wire from the src folder into the www folder
+echo "Wiring the src folder into the /var/www/html folder."
+echo "Start apache with: apache2ctl start"
+sudo chmod a+x $(pwd)/src && sudo rm -rf /var/www/html && sudo ln -s $(pwd)/src /var/www/html
